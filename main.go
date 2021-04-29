ae85633a7cf169f039dfe564c99d8f2188cee010
Nika Shamiladze <shamgoingzuck@gmail.com>
2021-04-29 22:21:41 +0400

    Update main.go

--- main.go
+++ main.go
@@ -4,9 +4,46 @@ import (
 	"fmt"
 	"bufio"
 	"os"
-	"strconv"
+	// "strconv"
 )
 
 func main() {
-	fmt.Println("Hello Go Calculator");
+	// scanner := bufio.NewScanner(os.Stdin);
+	// scanner.Scan()
+	// input, _ := strconv.ParseInt(scanner.Text(), 10, 64);
+	// fmt.Printf("You typed: %d", 2020-input);
+
+	// first scanner to identify username
+	fmt.Println("Hello, What's your name?");
+	scanner := bufio.NewScanner(os.Stdin);
+	scanner.Scan()
+	input := scanner.Text()
+
+	if input != "" && input != " " {
+		fmt.Printf("Hi " + input + "\n");
+	} else {
+		fmt.Println("Please type your name");
+	}
+
+	// second scanner to continue [Y/N]
+
+	fmt.Println("You Want Continue? [Y/N]");
+	continueScanner := bufio.NewScanner(os.Stdin)
+	continueScanner.Scan()
+	continueInput := continueScanner.Text()
+	if continueInput == "Y" || continueInput == "y" {
+
+	} else if continueInput == "N" || continueInput == "n" {
+		fmt.Printf("You Choose: " + continueInput + " Thanks for using calculator");
+	} else {
+		fmt.Printf("That's bad choose format " + continueInput + " Bye.");
+	}
+
+
+
+	// fmt.Printf("You Want Continue? [Y/N]");
+	// inpScanner := bufio.NewScanner(os.Stdin);
+	// inpScanner.Scan()
+	// inp := inpScanner.Text()
+	// fmt.Printf(inp);
 }
